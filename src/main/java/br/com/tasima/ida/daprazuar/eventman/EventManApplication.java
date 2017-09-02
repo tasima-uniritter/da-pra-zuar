3631e63d275ff35541727d723049b266925879b4
Renan Drabach <rmdrabach@gmail.com>
2017-09-02 09:00:13 -0300

    Adiciona um projeto gerado pelo Spring Boot Starter com os pacotes Web, DevTools, JPA e H2. O projeto está compilando e rodando (utilizando Eclipse).

---
+++ src/main/java/br/com/tasima/ida/daprazuar/eventman/EventManApplication.java
@@ -0,0 +1,12 @@
+package br.com.tasima.ida.daprazuar.eventman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManApplication.class, args);
+	}
+}
