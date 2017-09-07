aace98555bafee9c0b1f37c76139369118b8b006
Marcelo Miguel Alves <marmalves@gmail.com>
2017-09-07 15:31:26 -0300

    Criada a classe ClienteEvento para representar os clientes dos eventos
    Criada a classe/test case ClienteEventoTeste para testes de unidade

---
+++ src/main/java/br/com/tasima/ida/daprazuar/eventman/models/ClienteEvento.java
@@ -0,0 +1,10 @@
+package br.com.tasima.ida.daprazuar.eventman.models;
+
+public class ClienteEvento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/br/com/tasima/ida/daprazuar/eventman/ClienteEventoTeste.java
@@ -0,0 +1,14 @@
+package br.com.tasima.ida.daprazuar.eventman;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteEventoTeste {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
