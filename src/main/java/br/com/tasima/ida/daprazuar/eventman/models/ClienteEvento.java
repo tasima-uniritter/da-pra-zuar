f132e7b0f047c6709db623bed80e7a66f0cd427d
Marcelo Miguel Alves <marmalves@gmail.com>
2017-09-07 18:01:41 -0300

    Criada a classe para representar os Ingressos.
    Criada a classe/test case IngressoEventoTeste.
    
    Criados os métodos getters e setters das classes ClienteEvento e
    IngressoEvento.

--- src/main/java/br/com/tasima/ida/daprazuar/eventman/models/ClienteEvento.java
+++ src/main/java/br/com/tasima/ida/daprazuar/eventman/models/ClienteEvento.java
@@ -1,10 +1,27 @@
 package br.com.tasima.ida.daprazuar.eventman.models;
 
 public class ClienteEvento {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	String nome;
+    String email;
+    String telefone;
+	private String getNome() {
+		return nome;
 	}
-
+	private void setNome(String nome) {
+		this.nome = nome;
+	}
+	private String getEmail() {
+		return email;
+	}
+	private void setEmail(String email) {
+		this.email = email;
+	}
+	private String getTelefone() {
+		return telefone;
+	}
+	private void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+    
 }
