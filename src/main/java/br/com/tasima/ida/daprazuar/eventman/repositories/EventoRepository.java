363815444a4a68ed8f9b925c4601d97cc8d70abc
Marcelo Miguel Alves <marmalves@gmail.com>
2017-09-25 11:33:11 -0300

    Criada interface EventoRepository.

--- src/main/java/br/com/tasima/ida/daprazuar/eventman/repositories/EventoRepository.java
+++ src/main/java/br/com/tasima/ida/daprazuar/eventman/repositories/EventoRepository.java
@@ -2,45 +2,14 @@ package br.com.tasima.ida.daprazuar.eventman.repositories;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.tasima.ida.daprazuar.eventman.MemoryStorage;
 import br.com.tasima.ida.daprazuar.eventman.models.Evento;
 
 @Repository
-public class EventoRepository {
+public interface EventoRepository extends CrudRepository<Evento, Long>  {
 
-	private MemoryStorage memStorage;
 	
-	public EventoRepository() {
-		memStorage = MemoryStorage.getInstance();
-	}
-	
-	public List<Evento> FindAll() {
-		return memStorage.eventos;
-	}
-	
-	public Evento Find(int id) {
-		if (id > memStorage.eventos.size()-1)
-    		return null;
-    	
-    	return memStorage.eventos.get(id);
-	}
-	
-	public Evento Find(String name) {
-		Evento found = null;
-    	
-    	for (Evento e : memStorage.eventos) {
-			if (e.getNome().equalsIgnoreCase(name)) {
-				found = e;
-				break;
-			}
-		}
-    	
-    	return found;
-	}
-	
-	public void Insert(Evento ev) {
-		memStorage.eventos.add(ev);
-	}
 }
