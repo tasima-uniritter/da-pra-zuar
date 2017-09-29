3def599d97cd5c75d1c955d71db1a7263f2ef789
Marcelo Miguel Alves <marmalves@gmail.com>
2017-09-28 22:36:41 -0300

    Incluídos métodos de testes para validar quantidade máxima de caracteres do campo nome do evento e para validar se a data do evento é igual ou maior que a data atual.

--- src/test/java/br/com/tasima/ida/daprazuar/eventman/EventoTeste.java
+++ src/test/java/br/com/tasima/ida/daprazuar/eventman/EventoTeste.java
@@ -10,19 +10,45 @@ import br.com.tasima.ida.daprazuar.eventman.models.*;
 
 
 public class EventoTeste {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
 	@Test
 	public void testNovoEvento() {
 		Evento evento = new Evento();
 		assertEquals(true,true);
 	}
+	
+	@Test
+	public void nomeEventoNãoPodeTerMaisQue150Caracteres() {
+		 {
+		    String nome = "Informe nome do evento:";
+		    nome = JOptionPane.showInputDialog(null,nome);
+		    
+		    if (nome.length() > 150)  {
+		        nome = "Nome invalido";
+		        JOptionPane.showMessageDialog(null,nome,"Erro",0);
+		        System.exit(0);
+		    }
+		    
+		    assertEquals(true,true);
+		
+	}
 
+	@Test 
+	public void dataEventoDeveSerIgualOuMaiorQueDataDeHoje() {  
+
+		Calendar antes = Calendar.getInstance();
+        antes.set(Calendar.YEAR, 2000);
+        Calendar agora = Calendar.getInstance();
+        if(antes.before(agora)){
+            System.out.println("a data(antes) é anterior a data atual(agora)");
+        }
+        antes.set(Calendar.YEAR, 2020);
+        if(antes.after(agora)){
+            System.out.println("a data(antes) é posterior a data atual(agora)");
+        }
+	   
+        assertEquals(true,true);  
+        
+     }		 
+	
 }
